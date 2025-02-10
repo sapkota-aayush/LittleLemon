a154dd4fe341d0d9b368b3820eee4b9156ec6834
Aayush Sapkota <aayushsapkota1030@gmail.com>
2025-02-10 12:23:12 -0500

    Djsoer intergration

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from restaurant.views import CategoryViewSet, MenuItemViewSet, CartViewSet, OrderViewSet
-from restaurant.views import manager_view  # Import the new view
+from restaurant.views import (
+    CategoryViewSet, MenuItemViewSet, CartViewSet, OrderViewSet, manager_view
+)
 
-
-# Create a router & register our ViewSets
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'menu-items', MenuItemViewSet)
 router.register(r'cart', CartViewSet)
 router.register(r'orders', OrderViewSet)
 
-# Include router URLs
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Ensure this line is included for the admin panel
-    path('', include(router.urls)),  # This includes all API routes from the router
-    path('manager-only/', manager_view),  # New route for manager-only access
+    path('admin/', admin.site.urls),
+    # Djoser endpoints for user management and token authentication:
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    # Your API endpoints:
+    path('', include(router.urls)),
+    path('manager-only/', manager_view, name="manager-view"),
 ]
